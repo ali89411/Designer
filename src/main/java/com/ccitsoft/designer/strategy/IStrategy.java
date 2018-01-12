a4e7b263ffdf0442339ef9eed46bf3a28828915a
ali89411 <894118853@qq.com>
2018-01-12 17:28:37 +0800

    策略模式接口

---
+++ src/main/java/com/ccitsoft/designer/strategy/IStrategy.java
@@ -0,0 +1,17 @@
+package com.ccitsoft.designer.strategy;
+/***
+ * <p>Description: [首先定一个策略接口，这是诸葛亮老人家给赵云的三个锦囊妙计的接口]</p>
+ * Created on 2018年1月12日
+ * @author  <a href="mailto: lichao8941188@163.com">ali</a>
+ * @version 1.0 
+ * Copyright (c) 2018
+ */
+public interface IStrategy {
+
+	/**
+	 * <p>Discription:[每个锦囊妙计都是一个可执行的算法]</p>
+	 * Created on 2018年1月12日
+	 * @author:[ali]
+	 */
+	void operate();
+}
